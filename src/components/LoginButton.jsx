bdfc37d062be4b51143ce1f946d2717334c82aaa
Chaahat <arorac2@mymacewan.ca>
2023-07-09 21:24:26 -0600

    Fix react router to work correctly

--- src/components/LoginButton.jsx
+++ src/components/LoginButton.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import '../styles/LoginButton.css';
 import Button from '@mui/material/Button';
 
@@ -9,8 +8,18 @@ const LoginButton = () => {
       <a href='https://api.nextup.rocks/login'>
         <Button variant='contained'>Login</Button>
       </a>
+      {/* create a check and in the check add the link tag to the hosts page
+      if the status code 200, redirect. */}
     </div>
   );
 };
 
 export default LoginButton;
+
+// Create a check if return status 200, redirect to hostpage.or link to /hosts
+
+// Create a check if return status 200, redirect to hostpage.
+//     <Link href='/hosts'>
+//<Button variant='contained'>Login</Button>
+//</Link>
+//import { Link } from '@mui/material';
--- src/index.js
+++ src/index.js
@@ -1,26 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import ErrorPage from './components/ErrorPage';
+import HostPage from './pages/HostPage'
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <ErrorPage />,
+//   },
+// ]);
 
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <RouterProvider router={router} />
+//   </React.StrictMode>
+// );
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/hosts' element={<HostPage />} />
 
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 
 reportWebVitals();
