6a15485de7ae72998ef5470573476663e315651e
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-10 13:31:32 -0700

    install MUI and create login button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
-import logo from './images/NextUpLogo.png'; 
+import logo from './images/NextUpLogo.png';
 import './App.css';
 import React from 'react';
+import LoginButton from './components/LoginButton';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
+        <LoginButton />
       </header>
     </div>
   );
