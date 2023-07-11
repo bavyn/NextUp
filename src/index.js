1d500ab5178a499d296933dc806494047c60bb47
Karma Mann <129702902+bavyn@users.noreply.github.com>
2023-07-10 19:39:40 -0700

    create route to HostPage after logging in

--- src/index.js
+++ src/index.js
@@ -4,34 +4,18 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import HostPage from './pages/HostPage'
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <App />,
-//     errorElement: <ErrorPage />,
-//   },
-// ]);
-
-// ReactDOM.createRoot(document.getElementById("root")).render(
-//   <React.StrictMode>
-//     <RouterProvider router={router} />
-//   </React.StrictMode>
-// );
+import HostPage from './pages/HostPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<App />} />
-        <Route path='/hosts' element={<HostPage />} />
-
+        <Route path='/hosts/:userId' element={<HostPage />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
-
 reportWebVitals();
-
