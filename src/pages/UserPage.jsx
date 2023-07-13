39ae9ec710e32c76e6e85b1294369a9e2e021c7e
Chaahat <arorac2@mymacewan.ca>
2023-07-13 11:38:27 -0600

    add animation to UserPage and User welcome header

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import '../styles/UserPage.css';
-import TopNavBar from '../components/TopNavBar';
+import UserNavBar from '../components/UserNavBar';
 import SearchBar from '../components/SearchBar';
+import { useParams } from 'react-router-dom';
 
 const UserPage = () => {
+  const { userId } = useParams();
   const handleSearch = (query) => {
     const searchUrl = `https://open.spotify.com/search/${encodeURIComponent(query)}`;
     window.location.href = searchUrl;
@@ -12,7 +14,7 @@ const UserPage = () => {
   return (
     <div className='user-page'>
       <header className='user-page-header'>
-        <TopNavBar />
+        <UserNavBar userId={userId} />
       </header>
       <div className='user-page-body'>
         <div className='user-page-search-bar'>
