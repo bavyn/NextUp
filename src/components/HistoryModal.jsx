fa832a8e55b1fc9b87804dde2c2c149b0f397e02
Chaahat <arorac2@mymacewan.ca>
2023-07-17 02:03:52 -0600

    update history button style

--- src/components/HistoryModal.jsx
+++ src/components/HistoryModal.jsx
@@ -12,6 +12,7 @@ import {
 } from '@mui/material';
 import PropTypes from 'prop-types';
 import axios from 'axios';
+import ExportPlaylistButton from '../components/ExportPlaylistButton';
 
 const HistoryModal = ({ open, onClose, userId }) => {
   const [history, setHistory] = useState([]);
@@ -29,7 +30,7 @@ const HistoryModal = ({ open, onClose, userId }) => {
     <div className='history-modal'>
       <Dialog open={open} onClose={onClose}>
         <DialogTitle>
-          <h4 className='history-modal-header'>View history</h4>
+          <h4 className='history-modal-header' style={{ justifyContent: 'center', alignItems: 'center' }}>View history</h4>
         </DialogTitle>
 
         <DialogContent>
@@ -41,9 +42,8 @@ const HistoryModal = ({ open, onClose, userId }) => {
                 <ListItem key={song.id}>
                   <ListItemText
                     primary={song.Track?.name || 'Unknown Track'}
-                    secondary={`${song.Track.Artist.name || 'Unknown Artist'} - ${
-                      song.Track.Album.name || 'Unknown Album'
-                    }`}
+                    secondary={`${song.Track.Artist.name || 'Unknown Artist'} - ${song.Track.Album.name || 'Unknown Album'
+                      }`}
                   />
                 </ListItem>
               ))}
@@ -51,6 +51,7 @@ const HistoryModal = ({ open, onClose, userId }) => {
           )}
         </DialogContent>
         <DialogActions>
+          <ExportPlaylistButton userId={userId} />
           <Button onClick={onClose}>Close</Button>
         </DialogActions>
       </Dialog>
